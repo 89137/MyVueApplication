7ed99449eae7c3d37b8623c4015529f7d5931e09
Mattia Warning <ROB8001@robeco.nl>
2025-04-22 09:47:50 +0200

    added set

--- api/set-headers.js
+++ api/set-headers.js
@@ -2,4 +2,5 @@ export default (req, res) => {
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
   res.status(200).send('Headers Set')
+  
 }
